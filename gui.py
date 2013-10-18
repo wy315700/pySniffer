8750164e1328eac6e7a03305fd7590cf656a2742
wangyang <wy315700@gmail.com>
2013-10-18 13:46:52 +0800

    改成多进程
    添加树形图

--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ import socket
 import win_inet_pton
 import string, threading, time
 import sys, glob, random
+import multiprocessing, pickle
 
 def time2stamp(timestr, format_type='%Y-%m-%d %H:%M:%S'):
     return time.mktime(time.strptime(timestr, format_type))
@@ -61,24 +62,34 @@ class DemoFrame(wx.Frame):
                           u"pySniffer powerd by 天外之音",
                           size=(1000,400))
 
-        # il = wx.ImageList(16,16, True)
-        # for name in glob.glob("smicon??.png"):
-        #     bmp = wx.Bitmap(name, wx.BITMAP_TYPE_PNG)
-        #     il_max = il.Add(bmp)
-        self.list = wx.ListCtrl(self, -1, style=wx.LC_REPORT | wx.LC_HRULES)#创建列表
-        # self.list.AssignImageList(il, wx.IMAGE_LIST_SMALL)
+        
+        self.rootPanel = wx.Panel(self)
+        self.drawList(self.rootPanel);
+        self.tree = wx.TreeCtrl(self.rootPanel)
+        self.drawTextCtrl(self.rootPanel)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        vbox.Add(self.list, 1, wx.EXPAND | wx.ALL | wx.ALIGN_TOP, 20)
+        vbox.Add(self.tree, 1, wx.EXPAND | wx.ALL ^wx.TOP | wx.ALIGN_TOP, 20)
+
+        self.rootPanel.SetSizer(vbox)
+        self.num = 0
+    def drawTextCtrl(self,panel):
+        pass
+    def drawTreeCtrl(self,panel):
+        root = self.tree.AddRoot(u"第xx个数据包")
+        childId1 = self.tree.AppendItem(root, u"链路层数据")
+        childId2 = self.tree.AppendItem(root, u"网络层数据")
+        childId3 = self.tree.AppendItem(root, u"应用层数据")
+        pass
+    def drawList(self,panel): #创建主监视列表
+        self.list = wx.ListCtrl(panel, -1, style=wx.LC_REPORT | wx.LC_HRULES)#创建列表
 
         columns = [u"编号", u"时间" ,u"源网卡地址", u"目的网卡地址", u"协议", u"源IP", u"目的IP", u"源端口", u"目的端口"]
 
         # Add some columns
         for col in range(len(columns)):#增加列
             self.list.InsertColumn(col,columns[col])
-
-        # add the rows
-        # for item in range(50):#增加行
-        #     index = self.list.InsertStringItem(sys.maxint, 'aaa')
-                # self.list.SetStringItem(index, col+1, text)
-
                 
         # set the width of the columns in various ways
         self.list.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)#设置列的宽度
@@ -88,9 +99,6 @@ class DemoFrame(wx.Frame):
         self.list.SetColumnWidth(4, 40)
         self.list.SetColumnWidth(5, 100)
         self.list.SetColumnWidth(6, 100)
-
-        self.num = 0
-
     def addItem(self, itemList):
         self.num += 1
         index = self.list.InsertStringItem(sys.maxint, `self.num`)
@@ -116,13 +124,13 @@ def thread_print():
         while not isStop:
 
 
-
             ptime,pdata = global_queue.get()
 
             if isStop == True:
                 return
 
-            tem= dpkt.ethernet.Ethernet(pdata)
+            # tem= dpkt.ethernet.Ethernet(pdata)
+            tem = pdata #pdata不能被序列化 暂时在读取时处理
 
             global frame
 
@@ -163,34 +171,20 @@ def thread_print():
 
             frame.addItem(itemList)
 
-            # print "Package time is: ",ptime     
-
-            # print 'Source Mac Address is : ', eth_addr_to_str(tem.src)      
-
-            # print 'Dist   Mac Address is : ', eth_addr_to_str(tem.dst)      
-
-            # if tem.data.__class__.__name__=='IP':
-            #     src_ip  = '%d.%d.%d.%d'%tuple(map(ord,list(tem.data.src)))
-            #     dist_ip = '%d.%d.%d.%d'%tuple(map(ord,list(tem.data.dst)))
-            #     print 'Src   IP Address is : ', src_ip
-            #     print 'Dist  IP Address is : ', dist_ip
-            #     if tem.data.data.__class__.__name__ == 'TCP':
-            #         print "Dist Port is: ",tem.data.data.dport
-            #         print "Source Port is:",tem.data.data.sport
-            #         print "Content Data is:",tem.data.data.data
-            # print '\n'*3
-            # time.sleep(0.01)
     except KeyboardInterrupt:
         return
 
-def readFromPcap():
-    global global_queue, mutex, isStop
-    pc=pcap.pcap()
+
+def readFromPcap(global_queue):
+    # global global_queue, mutex, isStop
+    pc=pcap.pcap(immediate = True)
     # pc.setfilter()
     for ptime,pdata in pc:
-        if isStop:
-            return
-        global_queue.put((ptime,pdata))
+        # if isStop:
+        #     return
+        tem= dpkt.ethernet.Ethernet(pdata)#pdata不能被序列化 暂时在读取时处理
+        global_queue.put((ptime,tem))
+
         # print ptime
         # 释放锁
         # break
@@ -199,10 +193,10 @@ if __name__ == '__main__':
     isStop = False
 
 
-    global_queue = Queue.Queue(maxsize = 0)
-    mutex = threading.Lock()
+    global_queue = multiprocessing.Queue(maxsize = 0)
 
-    readThread = threading.Thread(target=readFromPcap, args=())
+    readThread = multiprocessing.Process(target=readFromPcap, args=(global_queue,))
+    # readThread = threading.Thread(target=readFromPcap, args=())
     readThread.start()
 
     app = wx.PySimpleApp()
@@ -212,11 +206,16 @@ if __name__ == '__main__':
     printThread = threading.Thread(target=thread_print, args=())
     printThread.start()
 
+
     app.MainLoop()
 
     isStop = True
 
     print 'Exit!!'
+    
+
+    readThread.terminate()
+    print 'TERMINATED:', readThread, readThread.is_alive()
 
     readThread.join()
     printThread.join()
